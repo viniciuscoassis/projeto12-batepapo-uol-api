080ce7a36559e4f0b3645da38ea1b67290c65b0c
Vinicius Assis <vinicius.coassis@gmail.com>
2022-09-01 09:22:50 -0300

    init

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(5000, () => console.log("Listening on 5000"));
